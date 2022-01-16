90b9d84f458535f2c797ff09723c6b95a5e01545
NunoBartolomeu <a47233@alunos.isel.pt>
2022-01-16 18:44:16 +0000

    Modelo da meste

---
+++ Modelo/Model.java
@@ -0,0 +1,71 @@
+import java.sql.*;
+import java.util.Scanner;
+
+public class Model {
+
+    private void printResults(ResultSet rs) throws SQLException {
+        final int TAB_SIZE = 8;
+        ResultSetMetaData meta = rs.getMetaData();
+        int columnsCount = meta.getColumnCount();
+        StringBuffer sep = new StringBuffer("\n");
+
+        // header
+        for (int i = 1; i <= columnsCount; i++) {
+            System.out.print(meta.getColumnLabel(i));
+            System.out.print("\t");
+            for (int j = 0; j < meta.getColumnDisplaySize(i)+TAB_SIZE; j++) {
+                sep.append('-');
+            }
+        }
+        System.out.println(sep);
+        // Step 4 - Get result
+        while (rs.next()) {
+            // results print
+            for (int i = 1; i <= columnsCount; i++) {
+                System.out.print(rs.getObject(i));
+                System.out.print("\t");
+            }
+            System.out.println();
+        }
+    }
+
+    private Connection getCon() throws SQLException {
+        return DriverManager.getConnection(App.getInstance().getConnectionString());
+    }
+
+    private void ListEmployee() {
+        //TODO: Implement
+        //System.out.println("ListEmployee()");
+        final String CMDST = "select fname, lname from employee where salary > ?;";
+        try {
+            Connection conn = getCon();
+            PreparedStatement pstmt = conn.prepareStatement(CMDST);
+            System.out.println("Insert salary");
+            java.util.Scanner key = new Scanner(System.in);
+            double salary = key.nextDouble();
+            pstmt.setDouble(1,salary); //
+            //2pstmt.executeUpdate();
+
+            // obter os resultados através de um select
+            printResults(pstmt.executeQuery());
+        }catch(SQLException err)
+        {
+            //Nothing to do. The option was not a valid one. Read another.
+        }
+    }
+    private void ListAverageSalary()
+    {
+        //TODO: Implement
+        System.out.println("ListAverageSalary()");
+    }
+    private void GetMaxSalary()
+    {
+        //TODO: Implement
+        System.out.println("GetMaxSalary()");
+    }
+    private void RegisterDependent()
+    {
+        //TODO: Implement
+        System.out.println("RegisterDependent()");
+    }
+}
