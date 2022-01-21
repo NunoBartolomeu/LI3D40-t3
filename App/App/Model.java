0b943e761f9c0b0dfeee713d895ec185fad35a7e
NunoBartolomeu <a47233@alunos.isel.pt>
2022-01-21 12:00:55 +0000

    defaulted all functions

--- App/App/Model.java
+++ App/App/Model.java
@@ -189,47 +189,70 @@ public class Model {
        }
     }
 
-    public static void substituirEquipa(){
-        System.out.println("substituirEquipa()");
 
-        final String query = "update pessoa set equipa = ? where id = ?; update pessoa set equipa = ? where id = ?;";
+    public static void substituirEquipa() throws SQLException{
+        //Connection
+        Connection conn = getCon();
         
-        try {
-            Connection conn = getCon();
-            PreparedStatement pstmt = conn.prepareStatement(query);
+        //Querys
+        final String query_updateEquipa = "update pessoa set equipa = ? where id = ?; update pessoa set equipa = ? where id = ?;";
+        
+        //Prepared Statements
+        PreparedStatement ps_updateSubstituir = conn.prepareStatement(query_updateEquipa);
+        PreparedStatement ps_updateSubstituta = conn.prepareStatement(query_updateEquipa);
+        
+
+        //ResultSet
+        ResultSet rs = null;
+
+        //Scanner
+        Scanner in = new Scanner(System.in);            
             
+        try {
+            /*
+                User Input
+            1 - Ask Question
+            2 - Register Answer
+            3 - Add to Prepared Statement
+            */
 
             System.out.print("Equipa (codigo) a receber a substituição: ");
-            java.util.Scanner key = new Scanner(System.in);
-            int equipa = key.nextInt();
-            pstmt.setInt(1, equipa);
-
+            int equipa = in.nextInt();
+            ps_updateSubstituir.setInt(1, equipa);
 
             System.out.print("Id da pessoa que vai substituir: ");
-            key = new Scanner(System.in);
-            int id1 = key.nextInt();
-            pstmt.setInt(2, id1);
-
+            int id1 = in.nextInt();
+            ps_updateSubstituir.setInt(2, id1);
 
             //equipa = 0;
-            pstmt.setInt(3, equipa);
+            ps_updateSubstituta.setInt(1, equipa);
 
-            
             System.out.print("Id da pessoa a ser substituida: ");
-            key = new Scanner(System.in);
-            int id2 = key.nextInt();
-            pstmt.setInt(4, id2);
+            int id2 = in.nextInt();
+            ps_updateSubstituta.setInt(2, id2);
 
-            System.out.print(pstmt);
-            //2pstmt.executeUpdate();
+            //Execute Querys
+            ps_updateSubstituta.executeUpdate();
 
-            // obter os resultados através de um select
-            pstmt.executeUpdate();
+            //Show Results
+            printResults(ps_updateSubstituta.executeQuery());
         }
-        
+
         catch(SQLException err){
-            //Nothing to do. The option was not a valid one. Read another.
+            System.out.println("Error detected: ");
+            System.out.println(err);
         }
+
+        finally{
+            //Close Result Set
+            if (rs != null) rs.close();
+
+            //Close all Prepared Statements
+            if (ps_updateSubstituta != null) ps_updateSubstituta.close();
+
+            //Close connection
+            if (conn != null) conn.close();
+       }
     }
 
     public static void desativarAtivo() throws SQLException{
@@ -290,164 +313,312 @@ public class Model {
        }
     }
 
-    public static void custoDeUmActivo() {
-        System.out.println("custoDeUmActivo()");
-/*
-        final String dtaquisicaoQuery = "select * from activo where id=?;";
-        final String valorComercialQuery = "select valor from vcomercial where activo=? and dtvcomercial=?;";
-        final String valorDasIntervencoesQuery = "select valcusto from intervencao where activo=? and dtvcomercial=?;";
+    public static void custoDeUmActivo() throws SQLException{
+        //Connection
+        Connection conn = getCon();
+        
+        //Querys
+        final String query_dtaquisicao = "select * from activo where id=?;";
+        final String query_valComer = "select valor from vcomercial where activo=? and dtvcomercial=?;";
+        final String query_valorInter = "select valcusto from intervencao where activo=? and dtvcomercial=?;";
 
+        //Prepared Statements
+        PreparedStatement ps_dtaquisicao = conn.prepareStatement(query_dtaquisicao);
+        PreparedStatement ps_valComer = conn.prepareStatement(query_valComer);
+        PreparedStatement ps_valInter = conn.prepareStatement(query_valorInter);
+        
+        //ResultSet
+        ResultSet rs = null;
+
+        //Scanner
+        Scanner in = new Scanner(System.in);            
+            
         try {
-            Connection conn = getCon();
-
-            PreparedStatement dtaquisicaoPS = conn.prepareStatement(dtaquisicaoQuery);
+            /*
+                User Input
+            1 - Ask Question
+            2 - Register Answer
+            3 - Add to Prepared Statement
+            */
+            
             System.out.println("Id do activo: ");
-            Scanner scanner = new Scanner(System.in);
-            String id = scanner.nextLine();
-            dtaquisicaoPS.setString(1, id);
+            String id = in.nextLine();
+            ps_dtaquisicao.setString(1, id);
             
 
-            PreparedStatement valorComercialPS = conn.prepareStatement(valorComercialQuery);
-            valorComercialPS.setString(1, id);
-            //String dtvComercial = scanner.nextLine();
-            valorComercialPS.setDate(2, java.sql.Date.valueOf(dtvComercial));
-            int valorComercial = valorComercialPS.executeQuery().getInt("valor");
+            ps_valComer.setString(1, id);
+            String dtvComercial = in.nextLine();
+            ps_valComer.setDate(2, java.sql.Date.valueOf(dtvComercial));
+            int valorComercial = ps_valComer.executeQuery().getInt("valor");
             
             
-            PreparedStatement intervencoesPS = conn.prepareStatement(valorDasIntervencoesQuery);
-            intervencoesPS.setString(1, id);
-            result = intervencoesPS.executeQuery(); 
+            ps_valInter.setString(1, id);
+            rs = ps_valInter.executeQuery(); 
             int valorIntervencoes = 0;
-            while (result.next()) {
-                valorIntervencoes += result.getInt("valcusto");
+            while (rs.next()) {
+                valorIntervencoes += rs.getInt("valcusto");
             }
             int total = valorComercial + valorIntervencoes;
             System.out.println("O activo tem um valor de: " + total + "€");
-        
-        } catch(SQLException err)
-        {
-            //Nothing to do. The option was not a valid one. Read another.
-        }*/
-    }
-
-    public static void pessoasIntervencao(){
-        System.out.println("pessoasIntervencao()");
-
-        final String query = "select pessoa.nome " +
-        "from activo join pessoa on activo.pessoa = pessoa.id " +
-        "where activo.nome =? " +
-        "union " +
-        "select p.nome " +
-        "from activo a join intervencao i on a.id = i.activo join inter_equipa ie on i.noint = ie.integerervencao join pessoa p on p.equipa = ie.equipa " +
-        "where a.nome =?;";
-
-        try {
-            Connection conn = getCon();
-
-            PreparedStatement PSactivo = conn.prepareStatement(query);
-            System.out.println("Nome do activo: ");
-            Scanner scanner = new Scanner(System.in);
-            String nome = scanner.nextLine();
-            PSactivo.setString(1, nome);
-            PSactivo.setString(2, nome);
-
-            printResults(PSactivo.executeQuery());
-        
-        } catch(SQLException err)
-        {
-            //Nothing to do. The option was not a valid one. Read another.
-        }
-    }
-
-    public static void ativosGeridosIntervidos(){
-        System.out.println("ativosGeridosIntervidos()");
-
-        final String query = "with manuel as ( " +
-            "select id, equipa " +
-                "from pessoa p " +
-                "where nome =? " +
-            "), " +
-           "geriu as ( " +
-            "select nome " +
-                "from activo a join manuel m on a.pessoa = m.id " +
-            ")"	+
-            "select nome " +
-            "from inter_equipa natural join manuel join intervencao on integerervencao = noint join activo on activo.id = activo "+
-            "union " +
-            "select nome " +
-            "from geriu;";
-
-        try {
-            Connection conn = getCon();
-
-            PreparedStatement activoGI = conn.prepareStatement(query);
-            System.out.println("Nome da pessoa: ");
-            Scanner scanner = new Scanner(System.in);
-            String nome = scanner.nextLine();
-            activoGI.setString(1, nome);
-
-            printResults(activoGI.executeQuery());
-        
-        } catch(SQLException err)
-        {
-            //Nothing to do. The option was not a valid one. Read another.
-        }
-    }
-
-    public static void responsaveisGestores(){
-        System.out.println("responsaveisGestores()");
-
-        final String query = "with responsaveis as ( " +
-        "select p.id, p.nome, p.profissao " +
-        "from pessoa p join equipa on p.id = responsavel " +
-        "), " +
-        "gestores as ( " +
-        "select r.id, r.nome, r.profissao " +
-        "from responsaveis r join activo on activo.pessoa = r.id " +
-        ")" +
-        "select distinct nome, profissao, telefone " +
-        "from gestores join tel_pessoa tp on gestores.id = tp.pessoa; ";
-
-
-        try {
-            Connection conn = getCon();
-
-            PreparedStatement respG = conn.prepareStatement(query);
             
-            printResults(respG.executeQuery());
-        
-        } catch(SQLException err)
-        {
-            //Nothing to do. The option was not a valid one. Read another.
+            //Execute Querys
+            ps_dtaquisicao.executeUpdate();
+
+            //Show Results
+            printResults(ps_dtaquisicao.executeQuery());
         }
+
+        catch(SQLException err){
+            System.out.println("Error detected: ");
+            System.out.println(err);
+        }
+
+        finally{
+            //Close Result Set
+            if (rs != null) rs.close();
+
+            //Close all Prepared Statements
+            if (ps_dtaquisicao != null) ps_dtaquisicao.close();
+
+            //Close connection
+            if (conn != null) conn.close();
+       }
     }
 
-    public static void intervencoesProg(){
-        System.out.println("intervencoesProg()");
+    public static void pessoasIntervencao() throws SQLException{
+        //Connection
+        Connection conn = getCon();
+        
+        //Querys
+        final String query_pessInter = "select pessoa.nome " +
+                                        "from activo join pessoa on activo.pessoa = pessoa.id " +
+                                        "where activo.nome =? " +
+                                        "union " +
+                                        "select p.nome " +
+                                        "from activo a join intervencao i on a.id = i.activo join inter_equipa ie on i.noint = ie.integerervencao join pessoa p on p.equipa = ie.equipa " +
+                                        "where a.nome =?;";
+        
+        //Prepared Statements
+        PreparedStatement ps_pessInter = conn.prepareStatement(query_pessInter);
+        
+        //ResultSet
+        ResultSet rs = null;
 
-        final String query = "select id, nome, descricao " +
+        //Scanner
+        Scanner in = new Scanner(System.in);            
+            
+        try {
+            /*
+                User Input
+            1 - Ask Question
+            2 - Register Answer
+            3 - Add to Prepared Statement
+            */
+            
+            System.out.println("Nome do activo: ");
+            String nome = in.nextLine();
+            ps_pessInter.setString(1, nome);
+            ps_pessInter.setString(2, nome);
+
+            //Execute Querys
+            ps_pessInter.executeUpdate();
+
+            //Show Results
+            printResults(ps_pessInter.executeQuery());
+        }
+
+        catch(SQLException err){
+            System.out.println("Error detected: ");
+            System.out.println(err);
+        }
+
+        finally{
+            //Close Result Set
+            if (rs != null) rs.close();
+
+            //Close all Prepared Statements
+            if (ps_pessInter != null) ps_pessInter.close();
+
+            //Close connection
+            if (conn != null) conn.close();
+       }
+    }
+
+    public static void ativosGeridosIntervidos() throws SQLException{
+        //Connection
+        Connection conn = getCon();
+        
+        //Querys
+        final String query_ativosGI = "with manuel as ( " +
+                                "select id, equipa " +
+                                    "from pessoa p " +
+                                    "where nome =? " +
+                                "), " +
+                               "geriu as ( " +
+                                "select nome " +
+                                    "from activo a join manuel m on a.pessoa = m.id " +
+                                ")"	+
+                                "select nome " +
+                                "from inter_equipa natural join manuel join intervencao on integerervencao = noint join activo on activo.id = activo "+
+                                "union " +
+                                "select nome " +
+                                "from geriu;";
+        
+        //Prepared Statements
+        PreparedStatement ps_ativosGI = conn.prepareStatement(query_ativosGI);
+        
+        //ResultSet
+        ResultSet rs = null;
+
+        //Scanner
+        Scanner in = new Scanner(System.in);            
+            
+        try {
+            /*
+                User Input
+            1 - Ask Question
+            2 - Register Answer
+            3 - Add to Prepared Statement
+            */
+            
+            System.out.println("Nome da pessoa: ");
+            String nome = in.nextLine();
+            ps_ativosGI.setString(1, nome);
+
+            //Execute Querys
+            ps_ativosGI.executeUpdate();
+
+            //Show Results
+            printResults(ps_ativosGI.executeQuery());
+        }
+
+        catch(SQLException err){
+            System.out.println("Error detected: ");
+            System.out.println(err);
+        }
+
+        finally{
+            //Close Result Set
+            if (rs != null) rs.close();
+
+            //Close all Prepared Statements
+            if (ps_ativosGI != null) ps_ativosGI.close();
+
+            //Close connection
+            if (conn != null) conn.close();
+       }
+    }
+
+    public static void responsaveisGestores() throws SQLException{
+        //Connection
+        Connection conn = getCon();
+        
+        //Querys
+        final String query_RespGest = "with responsaveis as ( " +
+                                "select p.id, p.nome, p.profissao " +
+                                "from pessoa p join equipa on p.id = responsavel " +
+                                "), " +
+                                "gestores as ( " +
+                                "select r.id, r.nome, r.profissao " +
+                                "from responsaveis r join activo on activo.pessoa = r.id " +
+                                ")" +
+                                "select distinct nome, profissao, telefone " +
+                                "from gestores join tel_pessoa tp on gestores.id = tp.pessoa; ";
+        
+        //Prepared Statements
+        PreparedStatement ps_RespGest = conn.prepareStatement(query_RespGest);
+        
+        //ResultSet
+        ResultSet rs = null;
+
+        //Scanner
+        Scanner in = new Scanner(System.in);            
+            
+        try {
+            /*
+                User Input
+            1 - Ask Question
+            2 - Register Answer
+            3 - Add to Prepared Statement
+            */
+
+            //Execute Querys
+            ps_RespGest.executeUpdate();
+
+            //Show Results
+            printResults(ps_RespGest.executeQuery());
+        }
+
+        catch(SQLException err){
+            System.out.println("Error detected: ");
+            System.out.println(err);
+        }
+
+        finally{
+            //Close Result Set
+            if (rs != null) rs.close();
+
+            //Close all Prepared Statements
+            if (ps_RespGest != null) ps_RespGest.close();
+
+            //Close connection
+            if (conn != null) conn.close();
+       }
+    }
+
+    public static void intervencoesProg() throws SQLException{
+        //Connection
+        Connection conn = getCon();
+        
+        //Querys
+        final String query_InterProg = "select id, nome, descricao " +
         "from activo inner join INTERVENCAO on id = activo " +
         "where (dtinicio > CURRENT_DATE and dtinicio < CURRENT_DATE + interval ? );";
         
+        //Prepared Statements
+        PreparedStatement ps_InterProg = conn.prepareStatement(query_InterProg);
+        
+        //ResultSet
+        ResultSet rs = null;
 
+        //Scanner
+        Scanner in = new Scanner(System.in);            
+            
         try {
-            Connection conn = getCon();
-
-            PreparedStatement intervencaoP = conn.prepareStatement(query);
+            /*
+                User Input
+            1 - Ask Question
+            2 - Register Answer
+            3 - Add to Prepared Statement
+            */
+            
             System.out.println("Inserir intervalo de tempo com unidades: year, month, week, day");
             System.out.println("Exemplo: '5 days', '2 months 3 days'");
             System.out.println("Intervalo de tempo: ");
-            Scanner scanner = new Scanner(System.in);
-            String intervalo = scanner.nextLine();
-            intervencaoP.setString(1, intervalo);
+            String intervalo = in.nextLine();
+            ps_InterProg.setString(1, intervalo);
 
-            System.out.println(intervencaoP);
-            
-            printResults(intervencaoP.executeQuery());
-        
-        } catch(SQLException err)
-        {
-            //Nothing to do. The option was not a valid one. Read another.
+            //Execute Querys
+            ps_InterProg.executeUpdate();
+
+            //Show Results
+            printResults(ps_InterProg.executeQuery());
         }
+
+        catch(SQLException err){
+            System.out.println("Error detected: ");
+            System.out.println(err);
+        }
+
+        finally{
+            //Close Result Set
+            if (rs != null) rs.close();
+
+            //Close all Prepared Statements
+            if (ps_InterProg != null) ps_InterProg.close();
+
+            //Close connection
+            if (conn != null) conn.close();
+       }
     }
 }
